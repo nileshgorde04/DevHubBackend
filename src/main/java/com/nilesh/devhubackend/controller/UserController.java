2135e5422fb9959997d1226663dd7d6ed40e0598
Nilesh Gorade <162355812+nileshgorde04@users.noreply.github.com>
2025-04-08 22:55:08 +0530

    Updated UserController.java

--- src/main/java/com/nilesh/devhubackend/controller/UserController.java
+++ src/main/java/com/nilesh/devhubackend/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:3000") // Allow frontend access
+@CrossOrigin(origins = "http://localhost:3000") 
 public class UserController {
     private final UserService userService;
 
